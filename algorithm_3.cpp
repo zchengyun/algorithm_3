88ffdea6b1e5985e64e1db1810a5f97a9ecd3155
zchengyun <1690132420@qq.com>
2021-09-08 00:05:40 +0800

    添加项目文件。

---
+++ algorithm_3.cpp
@@ -0,0 +1,21 @@
+﻿#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <ranges>
+//fill
+int main()
+{
+	std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	std::fill(v.begin(), v.end(), -1);
+
+	for (auto elem : v) {//-1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+		std::cout << elem << " ";
+	}
+	std::cout << "\n";
+
+	std::fill_n(v.begin(), 5, 6);
+	std::copy(begin(v), end(v), std::ostream_iterator<int>(std::cout, " "));//6 6 6 6 6 -1 -1 -1 -1 -1
+	std::cout << "\n";
+
+
+}
